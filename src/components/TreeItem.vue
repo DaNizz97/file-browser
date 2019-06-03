9def49ddb9d3c500bd0deed7ab351d3745072310
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-06-03 10:17:16 +0300

    Implement delay with progressbar

--- src/components/TreeItem.vue
+++ src/components/TreeItem.vue
@@ -3,12 +3,19 @@
     <div
         class="item2"
         :class="{bold: isFolder}"
-        @click="toggle()"
+        @click="toggleWithDelay()"
         @dblclick="createFile">
       <i v-if="isFolder && isOpen" class="material-icons-two-tone">folder</i>
       <i v-if="isFolder && !isOpen" class="material-icons">folder</i>
       <i v-if="!isFolder" class="material-icons-two-tone">file_copy</i>
       {{ item.name }}
+      <v-progress-circular
+          v-if="progress"
+          :size="18"
+          :width="3"
+          color="blue"
+          indeterminate
+      ></v-progress-circular>
     </div>
     <ul v-show="isOpen" v-if="isFolder">
       <tree-item
@@ -30,7 +37,8 @@
         },
         data: function () {
             return {
-                isOpen: false
+                isOpen: false,
+                progress: false
             }
         },
         computed: {
@@ -39,15 +47,27 @@
             }
         },
         methods: {
+            toggleWithDelay() {
+                if (!this.isOpen && this.isFolder) {
+                    this.progress = true;
+                    setTimeout(() => {
+                        this.toggle();
+                        this.progress = false;
+                    }, 1500)
+                } else {
+                    this.toggle();
+                }
+            },
+
             toggle: function () {
                 if (this.isFolder) {
                     this.isOpen = !this.isOpen
                 }
             },
             createFile() {
-              if (this.isFolder) {
-                  this.$emit('createFile', this.item)
-              }
+                if (this.isFolder) {
+                    this.$emit('createFile', this.item)
+                }
             }
         }
     }
@@ -61,6 +81,7 @@
     -webkit-font-smoothing: antialiased;
     text-rendering: optimizeLegibility;
   }
+
   .material-icons-two-tone {
     font-size: 16px;
     line-height: 1;
@@ -68,7 +89,10 @@
     -webkit-font-smoothing: antialiased;
     text-rendering: optimizeLegibility;
   }
-  .item2{}
+
+  .item2 {
+  }
+
   .item2:hover {
     cursor: pointer;
     background-color: aliceblue;
